735ac228acecc6645bdfa8fe49a7e01a7e05db87
lishikuan <lsm@lsmdeMacBook-Air.local>
2017-02-28 10:54:55 +0800

    冒泡排序

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -11,6 +11,23 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        NSLog(@"I am main.");
+        //冒泡排序
+        NSMutableArray * a =[NSMutableArray arrayWithObjects:@1,@2,@2,@5,@7,@4,@2, nil];
+        NSLog(@"before:%@",a);
+        int n = (int)a.count;
+        for (int j = 0; j < n - 1; j++){
+            for (int i = 0; i < n - 1 - j; i++)
+            {
+                if(a[i] > a[i + 1])
+                {
+                    [a exchangeObjectAtIndex:i withObjectAtIndex:i+1];
+                }
+            }
+        }
+        NSLog(@"exchanged:%@",a);
+    
+    
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HelloWorld
 My Hello World
+My name is Li Shikuan.This is my little app.Thank you!
